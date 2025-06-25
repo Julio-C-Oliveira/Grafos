4aee2cc07745a3daf6a6a47f77878578e8378488
Júlio C Oliveira <julio3.oliveir@gmail.com>
2025-06-25 18:13:27 -0300

    feat(bfs): Cálculo de distancia entre vértices para matrizes.

--- Algoritmos/bfs.h
+++ Algoritmos/bfs.h
@@ -16,5 +16,6 @@ void bfs_list(AdjacentGraph_list* graph, int startVertex);
 void bfs_matriz(AdjacentGraph_matriz* graph, int startVertex);
 
 int bfsCalculateStepSize_list(AdjacentGraph_list* graph, int startVertex, int endVertex);
+int bfsCalculateStepSize_matriz(AdjacentGraph_matriz* graph, int startVertex, int endVertex);
 
 #endif
