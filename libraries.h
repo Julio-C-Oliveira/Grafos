7dfea2b104d1775c8309143b232ab9171f9ff181
Júlio C Oliveira <julio3.oliveir@gmail.com>
2025-06-27 13:42:30 -0300

    style(Algoritmos): Mudei o nome da pasta para organizar melhor.

--- libraries.h
+++ libraries.h
@@ -3,7 +3,7 @@
 
 #include "Base/adjacentListGraph.h"
 #include "Base/adjacentMatrizGraph.h"
-#include "Algoritmos/dfs.h"
-#include "Algoritmos/bfs.h"
+#include "Algoritmos_de_Busca/dfs.h"
+#include "Algoritmos_de_Busca/bfs.h"
 
 #endif 
