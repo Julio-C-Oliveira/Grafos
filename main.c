15164a31fcd2763e75560b531f279443e0a30b0f
Júlio C Oliveira <julio3.oliveir@gmail.com>
2025-06-27 13:33:16 -0300

    refactor(main): Adiciona duas funções para rodar os testes.

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include "general.h"
 #include "libraries.h"
 
+
+void runTest_list(AdjacentGraph_list* graph, int startVertex, int endVertex) {
+    displayGraph_list(graph);
+    printf("\nNúmero de vértices: %d\n\n", getNumberOfVertices_list(graph));
+    dfs_list(graph, startVertex);
+    printf("\n");
+    bfs_list(graph, startVertex);
+    printf("\n");
+    printf("De %d para %d são necessários %d passos.\n", startVertex, endVertex, bfsCalculateStepSize_list(graph, startVertex, endVertex));
+    printf("\n");
+    dfsCycleSearch_list(graph, startVertex);
+}
+
+void runTest_matriz(AdjacentGraph_matriz* graph, int startVertex, int endVertex) {
+    displayGraph_matriz(graph);
+    printf("Número de vértices: %d\n\n", getNumberOfVertices_matriz(graph));
+    dfs_matriz(graph, 0);
+    printf("\n");
+    bfs_matriz(graph, 0);
+    printf("\n");
+    printf("De %d para %d são necessários %d passos.\n", startVertex, endVertex, bfsCalculateStepSize_matriz(graph, startVertex, endVertex));
+    printf("\n");
+    dfsCycleSearch_matriz(graph, startVertex);
+}
+
 int main() {
     int startVertex = 0; int endVertex = 4;
 
@@ -15,15 +40,7 @@ int main() {
     addEdge_list(adjacentListGraphNaoEuleriano, 0, 3, 1);
     addEdge_list(adjacentListGraphNaoEuleriano, 3, 4, 1);
 
-    displayGraph_list(adjacentListGraphNaoEuleriano);
-    printf("\nNúmero de vértices: %d\n\n", getNumberOfVertices_list(adjacentListGraphNaoEuleriano));
-    dfs_list(adjacentListGraphNaoEuleriano, startVertex);
-    printf("\n");
-    bfs_list(adjacentListGraphNaoEuleriano, startVertex);
-    printf("\n");
-    printf("De %d para %d são necessários %d passos.\n", startVertex, endVertex, bfsCalculateStepSize_list(adjacentListGraphNaoEuleriano, startVertex, endVertex));
-    printf("\n");
-    dfsCycleSearch_list(adjacentListGraphNaoEuleriano, startVertex);
+    runTest_list(adjacentListGraphNaoEuleriano, startVertex, endVertex);
     freeGraph_list(adjacentListGraphNaoEuleriano);
 
     printf("\nGrafo com matriz de adjacência: ====================================== ||\n");
@@ -36,15 +53,7 @@ int main() {
     addEdge_matriz(adjacentMatrizGraphNaoEuleriano, 0, 3, 1);
     addEdge_matriz(adjacentMatrizGraphNaoEuleriano, 3, 4, 1);
 
-    displayGraph_matriz(adjacentMatrizGraphNaoEuleriano);
-    printf("Número de vértices: %d\n\n", getNumberOfVertices_matriz(adjacentMatrizGraphNaoEuleriano));
-    dfs_matriz(adjacentMatrizGraphNaoEuleriano, 0);
-    printf("\n");
-    bfs_matriz(adjacentMatrizGraphNaoEuleriano, 0);
-    printf("\n");
-    printf("De %d para %d são necessários %d passos.\n", startVertex, endVertex, bfsCalculateStepSize_matriz(adjacentMatrizGraphNaoEuleriano, startVertex, endVertex));
-    printf("\n");
-    dfsCycleSearch_matriz(adjacentMatrizGraphNaoEuleriano, startVertex);
+    runTest_matriz(adjacentMatrizGraphNaoEuleriano, startVertex, endVertex);
     freeGraph_matriz(adjacentMatrizGraphNaoEuleriano);
 
     printf("\n\nCiclo Euleriano ====================================================================================== ||||\n");
@@ -58,15 +67,7 @@ int main() {
     addEdge_list(adjacentListGraphComCicloEuleriano, 0, 4, 1);
     addEdge_list(adjacentListGraphComCicloEuleriano, 3, 4, 1);
 
-    displayGraph_list(adjacentListGraphComCicloEuleriano);
-    printf("\nNúmero de vértices: %d\n\n", getNumberOfVertices_list(adjacentListGraphComCicloEuleriano));
-    dfs_list(adjacentListGraphComCicloEuleriano, 0);
-    printf("\n");
-    bfs_list(adjacentListGraphComCicloEuleriano, 0);
-    printf("\n");
-    printf("De %d para %d são necessários %d passos.\n", startVertex, endVertex, bfsCalculateStepSize_list(adjacentListGraphComCicloEuleriano, startVertex, endVertex));
-    printf("\n");
-    dfsCycleSearch_list(adjacentListGraphComCicloEuleriano, startVertex);
+    runTest_list(adjacentListGraphComCicloEuleriano, startVertex, endVertex);
     freeGraph_list(adjacentListGraphComCicloEuleriano);
 
     printf("\nGrafo com matriz de adjacência: ====================================== ||\n");
@@ -79,15 +80,7 @@ int main() {
     addEdge_matriz(adjacentMatrizGraphComCicloEuleriano, 0, 4, 1);
     addEdge_matriz(adjacentMatrizGraphComCicloEuleriano, 3, 4, 1);
 
-    displayGraph_matriz(adjacentMatrizGraphComCicloEuleriano);
-    printf("\nNúmero de vértices: %d\n\n", getNumberOfVertices_matriz(adjacentMatrizGraphComCicloEuleriano));
-    dfs_matriz(adjacentMatrizGraphComCicloEuleriano, 0);
-    printf("\n");
-    bfs_matriz(adjacentMatrizGraphComCicloEuleriano, 0);
-    printf("\n");
-    printf("De %d para %d são necessários %d passos.\n", startVertex, endVertex, bfsCalculateStepSize_matriz(adjacentMatrizGraphComCicloEuleriano, startVertex, endVertex));
-    printf("\n");
-    dfsCycleSearch_matriz(adjacentMatrizGraphComCicloEuleriano, startVertex);
+    runTest_matriz(adjacentMatrizGraphComCicloEuleriano, startVertex, endVertex);
     freeGraph_matriz(adjacentMatrizGraphComCicloEuleriano);
 
 
@@ -100,15 +93,7 @@ int main() {
     addEdge_list(adjacentListGraphComCaminhoEuleriano, 0, 3, 1);
     addEdge_list(adjacentListGraphComCaminhoEuleriano, 3, 4, 1);
 
-    displayGraph_list(adjacentListGraphComCaminhoEuleriano);
-    printf("\nNúmero de vértices: %d\n\n", getNumberOfVertices_list(adjacentListGraphComCaminhoEuleriano));
-    dfs_list(adjacentListGraphComCaminhoEuleriano, 0);
-    printf("\n");
-    bfs_list(adjacentListGraphComCaminhoEuleriano, 0);
-    printf("\n");
-    printf("De %d para %d são necessários %d passos.\n", startVertex, endVertex, bfsCalculateStepSize_list(adjacentListGraphComCaminhoEuleriano, startVertex, endVertex));
-    printf("\n");
-    dfsCycleSearch_list(adjacentListGraphComCaminhoEuleriano, startVertex);
+    runTest_list(adjacentListGraphComCaminhoEuleriano, startVertex, endVertex);
     freeGraph_list(adjacentListGraphComCaminhoEuleriano);
 
     printf("\nGrafo com matriz de adjacência: ====================================== ||\n");
@@ -119,15 +104,7 @@ int main() {
     addEdge_matriz(adjacentMatrizGraphComCaminhoEuleriano, 0, 3, 1);
     addEdge_matriz(adjacentMatrizGraphComCaminhoEuleriano, 3, 4, 1);
 
-    displayGraph_matriz(adjacentMatrizGraphComCaminhoEuleriano);
-    printf("\nNúmero de vértices: %d\n\n", getNumberOfVertices_matriz(adjacentMatrizGraphComCaminhoEuleriano));
-    dfs_matriz(adjacentMatrizGraphComCaminhoEuleriano, 0);
-    printf("\n");
-    bfs_matriz(adjacentMatrizGraphComCaminhoEuleriano, 0);
-    printf("\n");
-    printf("De %d para %d são necessários %d passos.\n", startVertex, endVertex, bfsCalculateStepSize_matriz(adjacentMatrizGraphComCaminhoEuleriano, startVertex, endVertex));
-    printf("\n");
-    dfsCycleSearch_matriz(adjacentMatrizGraphComCaminhoEuleriano, startVertex);
+    runTest_matriz(adjacentMatrizGraphComCaminhoEuleriano, startVertex, endVertex);
     freeGraph_matriz(adjacentMatrizGraphComCaminhoEuleriano);
     return 0;
 }
