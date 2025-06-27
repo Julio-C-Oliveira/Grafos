12b037b367bb3ae243f441df7f3ad153c2e13f67
Júlio C Oliveira <julio3.oliveir@gmail.com>
2025-06-27 14:03:19 -0300

    docs(Fleury): Adiciona pseudo-código pra servir de base.

---
+++ Algoritmos_Extras/fleury.c
@@ -0,0 +1,19 @@
+#include "fleury.h"
+
+/*
+Vou me basear nesse pseudo-código:
+Entrada: Grafo conexo G = (V, E)
+Saída: Um percurso fechado de Euler C
+
+Algoritmo:
+Escolher um vértice v qualquer de G; C <- {v}
+repita
+    Escolher uma aresta (v, w) não marcada via regra da ponte
+    atravessar (v,w); C <- C{w}
+    Marcar (v,w); v <- w
+até todas as arestas estarem marcadas;
+C <- C{v}; imprimir C
+
+Regra da Ponte: Se uma aresta (v,w) é uma ponte no grafo induzido pelas arestas não marcadas, 
+então (v,w) só deve ser escolhido pelo algoritmo caso não exista qualquer outra opção.
+*/
